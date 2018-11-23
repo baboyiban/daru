0bbb4f4970964aab8c42eb2f4eebd287c6a60a05
최다루한 <chl11wq12@gmail.com>
2018-11-23 23:05:49 +0900

    Initial Commit

---
+++ daru/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  daru
+//
+//  Created by 최다루한 on 23/11/2018.
+//  Copyright © 2018 최다루한. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
